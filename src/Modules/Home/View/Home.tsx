ae63688cbb57345ec1033be5262295b67b053710
Gunel1011 <gunelmm10@gmail.com>
2025-10-24 22:03:17 +0400

    CoffeeHarvest section apdate

--- src/Modules/Home/View/Home.tsx
+++ src/Modules/Home/View/Home.tsx
@@ -1,12 +1,14 @@
+import CoffeeHarvest from "./sections/coffeeHarvest";
 import Hero from "./sections/Hero";
 import OurOrigin from "./sections/OurOrigin";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Hero />
       <OurOrigin />
-    </div>
+      <CoffeeHarvest />
+    </>
   );
 };
 export default Home;
---
+++ src/Modules/Home/View/sections/CoffeeHarvest.tsx
@@ -0,0 +1,43 @@
+import coffe2 from "../../../../assets/video/coffe.2.mp4";
+import chery from "../../../../assets/img/cherry.webp";
+import Leaf from "../../../../assets/img/svg/fourLeafFlower.svg?react";
+const CoffeeHarvest = () => {
+  return (
+    <section className="harvest">
+      <div className="container">
+        {/* cery  */}
+        <div className="imgCerry">
+          <img src={chery} alt="chery" />
+        </div>
+        <div className="imgCerry top">
+          <img src={chery} alt="chery" />
+        </div>
+        <div className="imgCerry bottom">
+          <img src={chery} alt="chery" />
+        </div>
+        <div className="row">
+          {/* left side  */}
+          <div className="contentLefth">
+            <h2 className="text">SELECTIVELY HAND-PICKED, GRAIN BY GRAIN</h2>
+            <Leaf className="leaf" />
+          </div>
+          {/* video  */}
+          <div className="img">
+            <video id="videHarvest" autoPlay muted loop playsInline>
+              <source src={coffe2} type="video/mp4" />
+            </video>
+          </div>
+          {/* right side  */}
+          <div className="contentRigth">
+            <p className="text">
+              Proud of their heritage and the bounty of their sprawling green
+              lands
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CoffeeHarvest;
